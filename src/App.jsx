4fbf1750c1fa5621d2ff0283bb0d96cf3742be7f
Göktuğ <gokojapan5@gmail.com>
2022-12-12 16:57:17 +0300

    Project started

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import './styles/App.css';
+
+export const App = () => {
+  return <div className="min-h-screen bg-black/75">Your code goes hdere</div>;
+};
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import './styles/App.css';
-
-export const App = () => {
-  return (
-    <div className="app">
-      {/* Your code goes here */}
-    </div>
-  );
-}
